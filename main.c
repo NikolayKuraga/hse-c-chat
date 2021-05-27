ee9ad1caa1b506e13b40eec5b4764e0d9268faba
Олег Григорьев <o.grigoriev2@yandex.ru>
2021-05-27 19:08:28 +0300

    add command line stuff

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "server.h"
+#include "client.h"
+#include <iostream> //не убирать, эти библиотеки нужны для strcmp
+using namespace std;
 
-int main(int argc, char *argv[]) {
-    printf("Hello again!\n");
+int main(int argc, char** argv)
+{
+    if (strcmp(argv[1], "server") == 0)
+    {
+        printf("SERVER\n");
+    }
+    else {
+        printf("CLIENT\n");
+    }
+
     return 0;
 }
